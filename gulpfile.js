a719bbdda1b87333184f3da07442f6a55e0b574f
Ken Hawkins <khawkins98@gmail.com>
2019-10-17 10:57:50 +0200

    Tidy up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const gulp  = require('gulp');
-var through = require('through2');
+const through = require('through2');
+// Prepare eleventy
+process.argv.push('--config=eleventy.js'); // Eleventy config
+const elev = require('./eleventy-cmd.js');
+
+// Watch folders for changess
+gulp.task('watch', function() {
+  gulp.watch(['./src/**/*.{njk,html,js,md'], gulp.series('file-list', 'eleventy:reload'));
+});
 
 // Generate a sample list of all files in ./src
 // for demonstration Gulp integration with Eleventy
+// This list can be re-made by gulp and passed to Eleventy by way of 
+// src/site/_data/fileList.js
 gulp.task('file-list', function () {
-  global.fileList = [];
+  global.fileList = []; // there might be a better way make a variable to 11ty 
   return gulp.src(['./src/**/*.{njk,html,js,md}'])
     .pipe(through.obj(function (file, enc, cb) {
       global.fileList.push(file.path);
@@ -12,26 +22,20 @@ gulp.task('file-list', function () {
     }));
 });
 
-// Watch folders for changess
-gulp.task('watch', function() {
-  gulp.watch(['./src/**/*.{njk,html,js,md'], gulp.series('file-list', 'eleventy:reload'));
-});
-
-// Run eleventy
-process.argv.push('--config=eleventy.js'); // Eleventy config
-let elev = require('./eleventy-cmd.js');
-
+// Run elevent for local development
 gulp.task('eleventy:develop', function(done) {
   process.argv.push('--serve');
   process.env.ELEVENTY_ENV = 'development';
 
   // You could instead use elev.write() here, but then you should add your own browsersync task
   elev.watch().then(function() {
+    console.log('Eleventy loaded, serving to browser');
     elev.serve('3000');
     done();
   });
 });
 
+// Run eleventy as a static build
 gulp.task('eleventy:build', function(done) {
   process.argv.push('--quiet');
   process.env.ELEVENTY_ENV = 'production';
@@ -42,6 +46,9 @@ gulp.task('eleventy:build', function(done) {
   });
 });
 
+// Refresh eleventy
+// This is more thorough than elev.watch() as it will
+// also capture variable changes
 gulp.task('eleventy:reload', function(done) {
   elev.restart()
   elev.write()
@@ -64,5 +71,5 @@ gulp.task('build', gulp.series(
 gulp.task('dev', gulp.series(
   'file-list',
   'eleventy:develop',
-  gulp.parallel('watch')
+  'watch'
 ));
